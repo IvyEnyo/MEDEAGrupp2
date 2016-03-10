945a8a6586000825b26905733fc7960f05f01c5c
mackan92 <m.olofsson92@gmail.com>
2016-03-10 12:09:01 +0100

    Test

--- src/MedeaGUI3.java
+++ src/MedeaGUI3.java
@@ -1,9 +1,16 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.ImageIcon;
 
 public class MedeaGUI3 extends JFrame {
 
@@ -30,11 +37,21 @@ public class MedeaGUI3 extends JFrame {
 	 */
 	public MedeaGUI3() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		double width = screenSize.getWidth();
+		double height = screenSize.getHeight();
+		System.out.println("JFrame Width: " + width + " Height: " + height);
+		setBounds(0, 0, (int) width, (int) height);
+		setUndecorated(true);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon("/Users/Marcus/Documents/Programmering 2/MEDEAGrupp2/src/ppwf3.jpg"));
+		getContentPane().add(lblNewLabel, BorderLayout.NORTH);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
+		contentPane.setBackground(Color.DARK_GRAY);
+		contentPane.setBorder(new LineBorder(new Color(0, 0, 0)));
+		contentPane.setLayout(null);
+		
 	}
 
 }
