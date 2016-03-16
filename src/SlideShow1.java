4aa3c72e180ef433a83bbeafd3125b5fb6e4e118
IvyEnyo <ivyenyo040@gmail.com>
2016-03-16 14:05:26 +0100

    some change..
    not done

--- src/SlideShow1.java
+++ src/SlideShow1.java
@@ -12,25 +12,26 @@ public class SlideShow1 extends JFrame {
 	Timer tm; 
 	int x = 0; 
 	String [] list = {
+			//The list of Photos
 			"/Users/IvyEnyo/MEDEAGrupp2/src/ppwf2.jpg",
 			"/Users/IvyEnyo/MEDEAGrupp2/src/ppwf3.jpg",
 			"/Users/IvyEnyo/MEDEAGrupp2/src/ppwf1.jpg"
-			
 	};
 	
 	public SlideShow1() {
 		super("java SlideShow");
 		photo = new JLabel();
 		photo.setBounds(40, 30, 700, 300); 
-		SetImageSize(2); // the number of photos in the array
+		SetImageSize(2); // The number of photos in the array
 		
+		// 1000 is the delay
 		tm = new Timer(1000, new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				SetImageSize(x);
 				x +=1; 
-				if(x >= list.length)
+				if(x >= list.length - 1)
 					x = 0;
-			}
+			} 
 		});
 		add(photo); 
 		tm.start();
