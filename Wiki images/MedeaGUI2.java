05b8a2ba87cc48bb082b67725b6d455d13a2035b
madeleinenils <madeleinenilsson01@gmail.com>
2016-03-16 10:07:51 +0100

    Flyttat bilder från src till wiki

---
+++ Wiki images/MedeaGUI2.java
@@ -0,0 +1,57 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.ImageIcon;
+
+public class MedeaGUI2 extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MedeaGUI2 frame = new MedeaGUI2();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MedeaGUI2() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		double width = screenSize.getWidth();
+		double height = screenSize.getHeight();
+		System.out.println("JFrame Width: " + width + " Height: " + height);
+		setBounds(0, 0, (int) width, (int) height);
+		setUndecorated(true);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon("/Users/Marcus/Documents/Programmering 2/MEDEAGrupp2/src/ppwf2.jpg"));
+		getContentPane().add(lblNewLabel, BorderLayout.NORTH);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.DARK_GRAY);
+		contentPane.setBorder(new LineBorder(new Color(0, 0, 0)));
+		contentPane.setLayout(null);
+		
+	}
+
+}
