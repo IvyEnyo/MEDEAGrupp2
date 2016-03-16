611ac91fba6d51d422d4e9e4028b207e3a901f43
hermanwikner <wiknerherman@hotmail.com>
2016-03-16 11:37:26 +0100

    fix

--- src/MEDEASteerGUI.java
+++ src/MEDEASteerGUI.java
@@ -10,13 +10,17 @@ import javax.swing.ImageIcon;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Toolkit;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class MEDEASteerGUI extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
+	private JTextField amneField;
+	private JTextField tidField;
+	private JTextField platsField;
 	private JLabel lblNewLabel_1;
 	private JLabel lblNewLabel_2;
 
@@ -43,49 +47,52 @@ public class MEDEASteerGUI extends JFrame {
 		setTitle("INFO SK\u00C4RMEN");
 		setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\Hampus\\Desktop\\Elipse workspace1\\Testing_GUI\\img\\ma1.png"));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 716, 503);
+		setBounds(100, 100, 1276, 940);
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.DARK_GRAY);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		textField = new JTextField();
-		textField.setBounds(164, 55, 308, 78);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		amneField = new JTextField();
+		amneField.setFont(new Font("Avenir", Font.PLAIN, 12));
+		amneField.setBounds(189, 53, 308, 78);
+		contentPane.add(amneField);
+		amneField.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(164, 178, 308, 78);
-		contentPane.add(textField_1);
-		textField_1.setColumns(10);
+		tidField = new JTextField();
+		tidField.setFont(new Font("Avenir", Font.PLAIN, 12));
+		tidField.setBounds(189, 176, 308, 78);
+		contentPane.add(tidField);
+		tidField.setColumns(10);
 		
-		textField_2 = new JTextField();
-		textField_2.setBounds(164, 287, 308, 78);
-		contentPane.add(textField_2);
-		textField_2.setColumns(10);
+		platsField = new JTextField();
+		platsField.setFont(new Font("Avenir", Font.PLAIN, 12));
+		platsField.setBounds(189, 285, 308, 78);
+		contentPane.add(platsField);
+		platsField.setColumns(10);
 		
 		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\Hampus\\Desktop\\Elipse workspace1\\Testing_GUI\\img\\MEDEA-MAH-LOGO-medium.png"));
-		lblNewLabel.setBounds(330, 375, 332, 78);
+		lblNewLabel.setIcon(new ImageIcon("/Users/hermanwikner/Desktop/Skola/Programmering II/GitHub/MEDEAGrupp2/MEDEA-MAH-LOGO-medium.png"));
+		lblNewLabel.setBounds(330, 374, 332, 78);
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblmne = new JLabel("\u00C4mne");
 		lblmne.setForeground(Color.WHITE);
 		lblmne.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblmne.setBounds(275, 29, 46, 14);
+		lblmne.setBounds(316, 28, 46, 14);
 		contentPane.add(lblmne);
 		
 		JLabel lblTid = new JLabel("Tid");
 		lblTid.setForeground(Color.WHITE);
 		lblTid.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblTid.setBounds(275, 144, 46, 14);
+		lblTid.setBounds(316, 143, 46, 14);
 		contentPane.add(lblTid);
 		
 		JLabel lblPlats = new JLabel("Plats");
 		lblPlats.setForeground(Color.WHITE);
 		lblPlats.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblPlats.setBounds(275, 262, 46, 14);
+		lblPlats.setBounds(316, 261, 46, 14);
 		contentPane.add(lblPlats);
 		
 		lblNewLabel_1 = new JLabel("INFO");
@@ -99,5 +106,36 @@ public class MEDEASteerGUI extends JFrame {
 		lblNewLabel_2.setForeground(Color.WHITE);
 		lblNewLabel_2.setBounds(581, 31, 81, 14);
 		contentPane.add(lblNewLabel_2);
+		
+		JTextArea skarmArea = new JTextArea();
+		skarmArea.setFont(new Font("Avenir", Font.PLAIN, 15));
+		skarmArea.setEditable(false);
+		skarmArea.setBounds(509, 84, 201, 238);
+		contentPane.add(skarmArea);
+		
+		JButton btnNewButton = new JButton("SPARA");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				
+				String amne = amneField.getText();
+				String tid = tidField.getText();
+				String plats = platsField.getText();
+				
+				amneField.setText("");
+				tidField.setText("");
+				platsField.setText("");
+				
+				skarmArea.append("Ämne: " + amne + "\n");
+				skarmArea.append("Tid: " + tid + "\n");
+				skarmArea.append("Plats: " + plats + "\n");
+				
+				
+			}
+		});
+		btnNewButton.setBounds(529, 334, 117, 29);
+		contentPane.add(btnNewButton);
+		
+	
 	}
 }
