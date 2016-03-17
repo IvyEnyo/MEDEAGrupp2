d6f9e19c011d4bedcd559117175aea3c62b937e5
IvyEnyo <ivyenyo040@gmail.com>
2016-03-17 08:53:58 +0100

    added comments to the code

--- src/SlideShow1.java
+++ src/SlideShow1.java
@@ -11,24 +11,28 @@ public class SlideShow1 extends JFrame {
 	JLabel photo; 
 	Timer tm; 
 	int x = 0; 
+	
+	// List of Photo. 
 	String [] list = {
 	
-			"/img/fundersam.jpg",
-			"/img/forvirrad.jpg",
-			"/img/hangande.jpg",
-			"/img/utstrackta.jpg" 
+			"/img/fundersam.jpg", // 0
+			"/img/forvirrad.jpg", // 1
+			"/img/hangande.jpg", // 2
+			"/img/utstrackta.jpg" // 3
 		
 	};
 	
 	public SlideShow1() {
-		super("java SlideShow");
+		super("java SlideShow"); // Name of the slide we don't need it.... 
 		photo = new JLabel();
-		photo.setBounds(0, 0, 405, 720); 
-		SetImageSize(3); // the number of photos in the array
+		photo.setBounds(0, 0, 405, 720);  // Change the size for the photo here... 
+		SetImageSize(3); // The last index in the array..
 
 		
-		// 1000 is the delay
+		// Timer(delay, action) <-- change delay for faster or slower slide. 
 		tm = new Timer(5000, new ActionListener() {
+			
+			/*Makes the animation for the slide...  */
 			public void actionPerformed(ActionEvent e) {
 				SetImageSize(x);
 				x +=1; 
@@ -47,9 +51,9 @@ public class SlideShow1 extends JFrame {
 	}
 	
 	public void SetImageSize(int i) {
-		ImageIcon icon = new ImageIcon(SlideShow1.class.getResource(list[i]));
+		ImageIcon icon = new ImageIcon(SlideShow1.class.getResource(list[i]));  // Calls the list of photos. 
 		Image img = icon.getImage();  
-		Image newImg = img.getScaledInstance(photo.getWidth(), photo.getHeight(), Image.SCALE_SMOOTH);
+		Image newImg = img.getScaledInstance(photo.getWidth(), photo.getHeight(), Image.SCALE_SMOOTH); 
 		ImageIcon newImc = new ImageIcon(newImg);
 		photo.setIcon(newImc);
 	}
