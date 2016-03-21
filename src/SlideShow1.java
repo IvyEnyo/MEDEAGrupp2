ddb616384722234f770302f25c35fdbb7d9e2306
IvyEnyo <ivyenyo040@gmail.com>
2016-03-21 13:44:00 +0100

    time add

--- src/SlideShow1.java
+++ src/SlideShow1.java
@@ -1,6 +1,7 @@
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Calendar;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -11,11 +12,14 @@ public class SlideShow1 extends JFrame {
 	JLabel photo; 
 	Timer tm; 
 	int x = 0; 
+	Calendar c = Calendar.getInstance();
+	int min = c.get(Calendar.MINUTE);
+	int sec = c.get(Calendar.SECOND);
 	
 	// List of Photo. 
 	String [] list = {
 	
-	"/img/Gubbe1.1.jpg",
+			"/img/Gubbe1.1.jpg",
 			"/img/utanbubbla.jpg",
 			"/img/Gubbe1.2.jpg",
 			"/img/utanbubbla.jpg", 
@@ -28,21 +32,27 @@ public class SlideShow1 extends JFrame {
 	public SlideShow1() {
 		//super("java SlideShow");
 		photo = new JLabel();
-		photo.setBounds(0, 0, 1080, 1920);
+		photo.setBounds(0, 0, 405, 720);
 		setUndecorated(true);
-		//SetImageSize(6); // the number of photos in the array
-
-		
+		//setImage(5); // the number of photos in the array 
+		 
 		// 5000 is the delay
 		tm = new Timer(3000, new ActionListener() {
-
 			public void actionPerformed(ActionEvent e) {
-				SetImageSize(x);
-				x +=1; 
-				if(x >= list.length)
+				
+				setImage(x);
+				x +=1;
+				min = c.get(Calendar.MINUTE);
+				sec = c.get(Calendar.SECOND);
+				if(min % 5 == 0 && sec < 10) {
 					x = 0;
-			} 
-		});
+				} 
+				
+				if(x >= list.length) {
+					x = 0;
+				}
+		}
+		}); 
 		add(photo); 
 		tm.start();
 		setLayout(null);
@@ -51,9 +61,11 @@ public class SlideShow1 extends JFrame {
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
-	}
+}
 	
-	public void SetImageSize(int i) {
+
+	
+	public void setImage(int i) {
 		ImageIcon icon = new ImageIcon(SlideShow1.class.getResource(list[i]));  // Calls the list of photos. 
 		Image img = icon.getImage();  
 		Image newImg = img.getScaledInstance(photo.getWidth(), photo.getHeight(), Image.SCALE_SMOOTH); 
@@ -61,9 +73,13 @@ public class SlideShow1 extends JFrame {
 		photo.setIcon(newImc);
 	}
 	
+
+	
 public static void main(String[] args) {
 	
 	new SlideShow1();
 }
+
+
 	
 }
