01ea821982e33f257ccb4b5351b6017d8c028f89
hermanwikner <wiknerherman@hotmail.com>
2016-03-21 13:58:33 +0100

    Fixade SlideShow 2. Exporterade Jar filer för test.

--- src/SlideShow2.java
+++ src/SlideShow2.java
@@ -1,6 +1,7 @@
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Calendar;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -11,55 +12,73 @@ public class SlideShow2 extends JFrame {
 	JLabel photo; 
 	Timer tm; 
 	int x = 0; 
-	String [] list = {
+	Calendar c = Calendar.getInstance();
+	int min = c.get(Calendar.MINUTE);
+	int sec = c.get(Calendar.SECOND);
 	
+	// List of Photo. 
+	String [] list = {
+			
 			"/img/utanbubblaspegel.jpg",
 			"/img/Gubbe2.1.jpg",
-			"/img/utanbubblaspegel.jpg", 
+			"/img/utanbubbla.jpg",
 			"/img/Gubbe2.2.jpg",
 			"/img/utanbubblaspegel.jpg", 
-			"/img/Gubbe2.3.jpg"		
+			"/img/Gubbe2.3.jpg"
+	
 	};
 	
-	public SlideShow2() {
+	public SlideShow2() {	
 		//super("java SlideShow");
 		photo = new JLabel();
-		photo.setBounds(0, 0, 1080, 1920);
+		photo.setBounds(0, 0, 405, 720);
 		setUndecorated(true);
-		//SetImageSize(6); // the number of photos in the array
-
-		
+		//setImage(5); // the number of photos in the array 
+		 
 		// 5000 is the delay
 		tm = new Timer(3000, new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				SetImageSize(x);
-				x +=1; 
-				if(x >= list.length)
+				
+				setImage(x);
+				x +=1;
+				min = c.get(Calendar.MINUTE);
+				sec = c.get(Calendar.SECOND);
+				if(min % 5 == 0 && sec < 10) {
 					x = 0;
-			} 
-		});
+				} 
+				
+				if(x >= list.length) {
+					x = 0;
+				}
+		}
+		}); 
 		add(photo); 
 		tm.start();
 		setLayout(null);
-		setSize(1080, 1920);
+		setSize(1080, 1920); 
 		getContentPane();
-		//setLocationRelativeTo(0, 0);
-		setLocation(0,0);
+		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
-	}
+}
 	
-	public void SetImageSize(int i) {
-		ImageIcon icon = new ImageIcon(SlideShow1.class.getResource(list[i]));
+
+	
+	public void setImage(int i) {
+		ImageIcon icon = new ImageIcon(SlideShow1.class.getResource(list[i]));  // Calls the list of photos. 
 		Image img = icon.getImage();  
-		Image newImg = img.getScaledInstance(photo.getWidth(), photo.getHeight(), Image.SCALE_SMOOTH);
+		Image newImg = img.getScaledInstance(photo.getWidth(), photo.getHeight(), Image.SCALE_SMOOTH); 
 		ImageIcon newImc = new ImageIcon(newImg);
 		photo.setIcon(newImc);
 	}
+	
+
 	
 public static void main(String[] args) {
 	
 	new SlideShow2();
 }
+
+
 	
 }
