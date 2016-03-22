92b3eea670fd55875b6cf03c1e321cfc48f4b956
IvyEnyo <ivyenyo040@gmail.com>
2016-03-22 11:49:54 +0100

    Set a delay for sync and clean code

--- src/SlideShow1.java
+++ src/SlideShow1.java
@@ -9,14 +9,16 @@ import javax.swing.JLabel;
 import javax.swing.Timer;
 
 public class SlideShow1 extends JFrame {
-	JLabel photo; 
-	Timer tm; 
-	int x = 0; 
+	JLabel photo;
+	Timer tm;
+	int x = 0;
+	
 	Calendar c = Calendar.getInstance();
 	int min = c.get(Calendar.MINUTE);
 	int sec = c.get(Calendar.SECOND);
+	int ms = c.get(Calendar.MILLISECOND);
 	
-	// List of Photo. 
+	// List of Photo.
 	String [] list = {
 	
 			"/img/Gubbe1.1.jpg",
@@ -30,34 +32,33 @@ public class SlideShow1 extends JFrame {
 	};
 	
 	public SlideShow1() {
-		//super("java SlideShow");
 		photo = new JLabel();
-		photo.setBounds(0, 0, 1080, 1920);
-		setUndecorated(true);
-		//setImage(5); // the number of photos in the array 
-		 
+		photo.setBounds(0, 0, 720, 800);
+		
 		// 5000 is the delay
 		tm = new Timer(3000, new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
 				setImage(x);
-				x +=1;
+				x += 1;
 				min = c.get(Calendar.MINUTE);
 				sec = c.get(Calendar.SECOND);
 				if(min % 3 == 0 && sec < 1) {
 					x = 0;
-				} 
+				}
 				
 				if(x >= list.length) {
 					x = 0;
 				}
 		}
-		}); 
-		add(photo); 
+	});
+		add(photo);
+		tm.setInitialDelay(60000 - ((sec * 1000) + ms)); // Sets a delay to start the slide show. 
 		tm.start();
 		setLayout(null);
-		setSize(1080, 1920); 
+		setSize(720, 800);
 		getContentPane();
+		setUndecorated(true);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
@@ -66,20 +67,16 @@ public class SlideShow1 extends JFrame {
 
 	
 	public void setImage(int i) {
-		ImageIcon icon = new ImageIcon(SlideShow1.class.getResource(list[i]));  // Calls the list of photos. 
-		Image img = icon.getImage();  
-		Image newImg = img.getScaledInstance(photo.getWidth(), photo.getHeight(), Image.SCALE_SMOOTH); 
+		ImageIcon icon = new ImageIcon(SlideShow1.class.getResource(list[i]));  // Calls the list of photos.
+		Image img = icon.getImage();
+		Image newImg = img.getScaledInstance(photo.getWidth(), photo.getHeight(), Image.SCALE_SMOOTH);
 		ImageIcon newImc = new ImageIcon(newImg);
 		photo.setIcon(newImc);
 	}
 	
 
 	
-public static void main(String[] args) {
-	
-	new SlideShow1();
-}
-
-
-	
+	public static void main(String[] args) {
+		new SlideShow1();
+	}
 }
--- src/SlideShow2.java
+++ src/SlideShow2.java
@@ -9,12 +9,14 @@ import javax.swing.JLabel;
 import javax.swing.Timer;
 
 public class SlideShow2 extends JFrame {
-	JLabel photo; 
-	Timer tm; 
-	int x = 0; 
+	JLabel photo;
+	Timer tm;
+	int x = 0;
+	
 	Calendar c = Calendar.getInstance();
 	int min = c.get(Calendar.MINUTE);
 	int sec = c.get(Calendar.SECOND);
+	int ms = c.get(Calendar.MILLISECOND);
 	
 	// List of Photo. 
 	String [] list = {
@@ -23,17 +25,14 @@ public class SlideShow2 extends JFrame {
 			"/img/Gubbe2.1.jpg",
 			"/img/utanbubblaspegel.jpg",
 			"/img/Gubbe2.2.jpg",
-			"/img/utanbubblaspegel.jpg", 
+			"/img/utanbubblaspegel.jpg",
 			"/img/Gubbe2.3.jpg"
 	
 	};
 	
-	public SlideShow2() {	
-		//super("java SlideShow");
+	public SlideShow2() {
 		photo = new JLabel();
-		photo.setBounds(0, 0, 1080, 1920);
-		setUndecorated(true);
-		//setImage(5); // the number of photos in the array 
+		photo.setBounds(0, 0, 720, 800);
 		 
 		// 5000 is the delay
 		tm = new Timer(3000, new ActionListener() {
@@ -45,39 +44,36 @@ public class SlideShow2 extends JFrame {
 				sec = c.get(Calendar.SECOND);
 				if(min % 3 == 0 && sec < 1) {
 					x = 0;
-				} 
+				}
 				
 				if(x >= list.length) {
 					x = 0;
 				}
 		}
-		}); 
-		add(photo); 
+	}); 
+		add(photo);
+		tm.setInitialDelay(60000 - ((sec * 1000) + ms)); // Sets a delay to start the slide show.
 		tm.start();
 		setLayout(null);
-		setSize(1080, 1920); 
+		setSize(720, 800);
 		getContentPane();
+		setUndecorated(true);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
 }
 	
-
-	
 	public void setImage(int i) {
-		ImageIcon icon = new ImageIcon(SlideShow1.class.getResource(list[i]));  // Calls the list of photos. 
-		Image img = icon.getImage();  
-		Image newImg = img.getScaledInstance(photo.getWidth(), photo.getHeight(), Image.SCALE_SMOOTH); 
+		ImageIcon icon = new ImageIcon(SlideShow1.class.getResource(list[i]));  // Calls the list of photos.
+		Image img = icon.getImage();
+		Image newImg = img.getScaledInstance(photo.getWidth(), photo.getHeight(), Image.SCALE_SMOOTH);
 		ImageIcon newImc = new ImageIcon(newImg);
 		photo.setIcon(newImc);
 	}
 	
-
-	
-public static void main(String[] args) {
-	
-	new SlideShow2();
-}
+	public static void main(String[] args) {
+		new SlideShow2();
+	}
 
 
 	
